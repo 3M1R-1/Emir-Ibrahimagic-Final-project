704faa0607a73da23378c8238dce6511982c35e7
3M1R-1 <emcho.ibrahimagic@gmail.com>
2025-04-30 20:16:49 +0200

    Update Final project Emir Ibrahimagić.py

--- Final project Emir Ibrahimagić.py
+++ Final project Emir Ibrahimagić.py
@@ -1,65 +1,91 @@
+
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+pitanja = [
+    "Koliko igrača čini jedan tim u košarci?",
+    "Ko je naslikao Mona Lisu?",
+    "Koje su tri osnovne boje? a=plava, crvena, žuta. b=plava, žuta, zelena. c=crvena, žuta, ljubičasta?",
+    "Koja je najveća planeta u Sunčevom sistemu?",
+    "Koja životinja može spavati naopačke?",
+    "Zašto astronauti lede u svemiru? a=nema kisika, b=nema gravitacije, c=previše gravitacije",
+    "Koji je hemijski simbol za vodu?",
+    "Koji je najtvrđi materijal na svijetu?",
+    "Koja je formula za izračunavanje gustine tijela?",
+    "Koji kontinent ima najviše država?",
+    "Ko se smatra osnivačem dinastije Kotromanića?",
+    "Ko je napisao Pinokija?",
+    "Koji je najpoznatiji web pretraživač?",
+    "Koliko iznosi pi (π) (bar 5 brojeva)?",
+    "Ko je najbolji šahista?"
+]
+
+odgovori = [
+    "5", "LEONARDO DA VINCI", "A", "JUPITER", "ŠIŠMIŠ",
+    "B", "H2O", "DIJAMANT", "M/V", "AFRIKA",
+    "STJEPAN KOTROMANIC", "KARLO KOLODI", "GOOGLE CHROME", "3.1415", "MAGNUS CARLSEN"
+]
+
+
 score = 0
-def provjeri_pitanje(odgovor, tačan_odgovor):
-    global score 
-    pokušaj = 3
-    while pokušaj > 0:
-      if(odgovor.upper() == tačan_odgovor.upper()):
-        print("Tačan odgovor")
+trenutno_pitanje = 0
+
+def provjeri_odgovor():
+    global score, trenutno_pitanje
+
+    odgovor = entry.get().upper()
+    if odgovor == odgovori[trenutno_pitanje]:
         score += 1
-        break
-      else:
-          print ("Netačan odgovor")
-          break
-      
-pitanje_1 = "Koliko igrača čini jedan tim u košarci?"
-pitanje_2 = "Ko je naslikao Mona Lisu?"
-pitanje_3 = "Koje su tri osnovne boje?, a=plava, crvena, žuta. b=plava, žuta, zelena. c=crvena, žuta, ljubičasta?"
-pitanje_4 = "Koja je najveća planeta u sunčevom sistemu?"
-pitanje_5 = "Koja životinja može spavati naopako?"
-pitanje_6 = "Zašto astronauti lebde u svemiru? a=nema kisika, b=nema gravitacije, c=previše gravitacije"
-pitanje_7 = "Koji je hemijski simbol za vodu?"
-pitanje_8 = "Koji je najtvrđi materijal na svijetu?"
-pitanje_9 = "Koja je formula za izračunavanje gustine tijela?"
-pitanje_10 = "Koji kontinent ima najviše država?"
-pitanje_11 = "Ko se smatra osnivačem dinastije Kotromanića?"
-pitanje_12 = "Ko je napisao ¨Pinokija¨?"
-Pitanje_13 = "Koji je najpoznatiji web pretraživač?"
-pitanje_14 = "Koliko iznosi pi(π)(bar 5 brojeva)?"
-pitanje_15 = "Ko je najbolji šahista?"
-lista_pitanja = (pitanje_1, pitanje_2, pitanje_3, pitanje_4, pitanje_5, pitanje_6, pitanje_7, pitanje_8, pitanje_9, pitanje_10, pitanje_11, pitanje_12, Pitanje_13, pitanje_14, pitanje_15)
-odgovor_1 = "5"
-odgovor_2 = "LEONARDO DA VINCI"
-odgovor_3 = "A"
-odgovor_4 = "JUPITER"
-odgovor_5 = "SISMIS"
-odgovor_6 = "B"
-odgovor_7 = "H2O"
-odgovor_8 = "DIJAMANT"
-odgovor_9 = "M/V"
-odgovor_10 = "AFRIKA"
-odgovor_11 = "STJEPAN KOTROMANIC"
-odgovor_12 = "KARLO KOLODI"
-odgovor_13 = "GOOGLE CHROME"
-odgovor_14 = "3.1415"
-odgovor_15 = "MAGNUS CARLSEN"
+        messagebox.showinfo("Tačno!", "Odgovor je tačan!")
+    else:
+        messagebox.showerror("Netačno", f"Netačan odgovor. Tačan je: {odgovori[trenutno_pitanje]}")
 
-lista_odgovora = (odgovor_1, odgovor_2, odgovor_3, odgovor_4, odgovor_5, odgovor_6, odgovor_7, odgovor_8, odgovor_9, odgovor_10, odgovor_11, odgovor_12, odgovor_13, odgovor_14, odgovor_15)
+    trenutno_pitanje += 1
 
-for i in range(15):
-    ulaz = input(lista_pitanja[i])
-    provjeri_pitanje(ulaz.upper(), lista_odgovora[i])
-print("Osvojili ste %s poena " % score)
+    if trenutno_pitanje < len(pitanja):
+        pitanje_label.config(text=pitanja[trenutno_pitanje])
+        entry.delete(0, tk.END)
+    else:
+        prikazi_rezultat()
 
-if score == 0:
-    print ("Igra preporučena za tebe je iks oks")
+def prikazi_rezultat():
+    root.destroy()
 
-if score > 0 and score < 6:
-    print ("Igra preporučena za tebe je fortnite")
+    rezultat = tk.Tk()
+    rezultat.title("Rezultat")
+    rezultat.geometry("400x200")
 
-if score > 5 and score < 11:
-    print ("Igra preporučena za tebe je roblox")
+    tekst = f"Osvojili ste {score} poena."
 
-if score > 10 and score < 16:
-    print ("Igra preporučena za tebe je šah")
+    if score == 0:
+        preporuka = "Igra preporučena za tebe je Iks Oks."
+    elif 0 < score < 6:
+        preporuka = "Igra preporučena za tebe je Fortnite."
+    elif 5 < score < 11:
+        preporuka = "Igra preporučena za tebe je Roblox."
+    else:
+        preporuka = "Igra preporučena za tebe je Šah."
+
+    label_rezultat = tk.Label(rezultat, text=tekst + "\n" + preporuka, font=("Arial", 14))
+    label_rezultat.pack(pady=50)
+
+    
 
 
+root = tk.Tk()
+root.title("Kviz")
+root.geometry("600x300")
+
+
+pitanje_label = tk.Label(root, text=pitanja[trenutno_pitanje], font=("Arial", 14))
+pitanje_label.pack(pady=20)
+
+
+entry = tk.Entry(root, font=("Arial", 14))
+entry.pack(pady=10)
+
+
+dugme = tk.Button(root, text="Potvrdi odgovor", command=provjeri_odgovor)
+dugme.pack(pady=20)
